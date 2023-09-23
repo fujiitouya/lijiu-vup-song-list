4368cad4eadc3b10aa76538552bda94413f103c6
kazusalove <a6087349@outlook.com>
2023-09-23 17:18:57 +0800

    update code
    update the first version.

---
+++ config/constants.js
@@ -0,0 +1,35 @@
+let config = {
+    Name: "黎九official",
+    
+    BiliLiveRoomID: "30222913",
+    
+    NetEaseMusicId: "",
+    QQMusicId: "",
+    Footer: "Copyright © 2023 黎九official 解释权归本直播间所有。",
+
+    Cursor: true,
+
+    LanguageCategories: ["【古风歌】","【高音】 ", "【现代/流行歌】","日语", "韩文", "【SC歌单】"],
+    RemarkCategories: [],
+
+    BannerTitle: "雪域proveamls所属 混乱之都代理城主",
+    BannerContent: [`混乱之都代理城主`,],
+
+    CustomButtons: [
+        {
+            link: "mailto:provealms@foxmail.com",
+            name: "商务合作",
+            image: ""
+        },
+        {
+            link: "https://space.bilibili.com/3493130540157780",
+            name: "录播组",
+            image: ""
+        }
+    ]
+}
+
+
+module.exports = {
+    config
+}
